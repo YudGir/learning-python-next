dbce445f5558b39cde8c647074ca04b28b2f1c7c
Yudha Reinhard Natanael Girsang <144704349+YudGir@users.noreply.github.com>
2026-06-01 23:09:02 +0700

    Update map.app.py

--- views/map.app.py
+++ views/map.app.py
@@ -15,7 +15,7 @@ geminiAPI = os.getenv("GEMINI_API_KEY") or st.secrets.get("GEMINI_API_KEY")
 DB_HOST = "aws-0-ap-southeast-1.pooler.supabase.com"
 DB_PORT = 6543
 DB_NAME = "postgres"
-DB_USER = "postgres.bhpiouzuqkoeyfainakj"
+DB_USER = "postgres.bhpiouzuqkoeyfainakj.session"
 DB_PASS = "IFPASTIBISA"
 
 def fetch_learnings_from_db():
