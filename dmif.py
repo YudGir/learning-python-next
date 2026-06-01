ad713683a84260ffa3ba5ae0fe889042f99f72ed
Yudha Reinhard Natanael Girsang <144704349+YudGir@users.noreply.github.com>
2026-06-01 23:36:48 +0700

    Update dmif.py

--- dmif.py
+++ dmif.py
@@ -1,9 +1,7 @@
 import streamlit as st
 
-# Setup konfigurasi halaman di paling atas file pengatur
 st.set_page_config(page_title="Web Application for DMIF", page_icon="🚀", layout="wide")
 
-# Definisikan halaman-halaman aplikasi menggunakan path absolut folder views
 pages = {
     "Menu Utama": [
         st.Page("views/map.app.py", title="IF Map Navigation", icon="🗺️"),
@@ -13,6 +11,5 @@ pages = {
     ]
 }
 
-# Jalankan navigasi
 pg = st.navigation(pages)
 pg.run()
