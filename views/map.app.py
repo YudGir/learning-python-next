ab1c4d66810b2b550b978a95d268ff2fc5578d6c
Yudha Reinhard Natanael Girsang <144704349+YudGir@users.noreply.github.com>
2026-06-01 23:12:50 +0700

    Update map.app.py

--- views/map.app.py
+++ views/map.app.py
@@ -11,11 +11,13 @@ from PIL import Image
 load_dotenv()
 geminiAPI = os.getenv("GEMINI_API_KEY") or st.secrets.get("GEMINI_API_KEY")
 
-# --- KUNCI PARAMETER INDIVIDUAL DI MAP_APP AGAR SINKRON DENGAN ADMIN ---
+# --- PARAMETER POOLER SAKTI ANTI-ENOTFOUND (SINKRONISASI TOTAL IPV6) ---
 DB_HOST = "aws-0-ap-southeast-1.pooler.supabase.com"
 DB_PORT = 6543
-DB_NAME = "postgres"
-DB_USER = "postgres.bhpiouzuqkoeyfainakj.session"
+DB_USER = "postgres.bhpiouzuqkoeyfainakj" # Gunakan format default gabungan ini
+
+# PERUBAHAN KRUSIAL: Ubah nama database dari "postgres" menjadi ID proyek unikmu!
+DB_NAME = "bhpiouzuqkoeyfainakj" 
 DB_PASS = "IFPASTIBISA"
 
 def fetch_learnings_from_db():
